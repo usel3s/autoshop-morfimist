843e976349f604a8d073809d98a5a567b600224e
morfiscam <96893585+morfiscam@users.noreply.github.com>
2023-03-11 18:54:50 +0300

    3.0 version

---
+++ developer bot morfiscam/tgbot/data/config.py
@@ -0,0 +1,35 @@
+# - *- coding: utf- 8 - *-
+import configparser
+
+read_config = configparser.ConfigParser()
+read_config.read("settings.ini")
+
+BOT_TOKEN = read_config['settings']['token'].strip().replace(" ", "")  
+PATH_DATABASE = "tgbot/data/database.db"  
+PATH_LOGS = "tgbot/data/logs.log"  
+BOT_VERSION = "3.0"  
+
+def get_admins():
+    read_admins = configparser.ConfigParser()
+    read_admins.read("settings.ini")
+
+    admins = read_admins['settings']['admin_id'].strip().replace(" ", "")
+
+    if "," in admins:
+        admins = admins.split(",")
+    else:
+        if len(admins) >= 1:
+            admins = [admins]
+        else:
+            admins = []
+
+    while "" in admins: admins.remove("")
+    while " " in admins: admins.remove(" ")
+    while "\r" in admins: admins.remove("\r")
+    while "\n" in admins: admins.remove("\n")
+
+    admins = list(map(int, admins))
+
+    return admins
+
+BOT_DESCRIPTION = 'MORFIMIST - https://github.com/morfiscam'
